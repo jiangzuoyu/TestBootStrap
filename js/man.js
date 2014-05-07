f911f6cc9e954813f5842de15dbdce9951acb7d6
zy.jiang <zuoyu@126.com>
2014-05-07 10:37:02 +0800

    aaa

--- js/man.js
+++ js/man.js
@@ -2,16 +2,4 @@ function foo2() {
 
 }
 
-<<<<<<< HEAD
-function bar() {
-    var aaa = 111;
-    var bbb = 222;
-    var ccc = 333;
 
-
-=======
-function bar(){
-  var aaa = 111;
-
->>>>>>> master
-}
